284baf079b9f98e2a76f22d4bc89b1f363fbc08b
Himanshu Gautam <1302399+hgautam@users.noreply.github.com>
2021-08-22 12:50:58 -0700

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example"
+	title := "Jenkins X imported project"
 
 	from := ""
 	if r.URL != nil {
